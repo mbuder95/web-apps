e5a693fae98a2f842e48d39c7c135d956ca4df84
Mark Buder <mark.buder@kellogg.northwestern.edu>
2024-02-08 03:31:43 +0000

    Web Apps Code Along

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  
+  get "/dice", :controller => "dice", 
+  :action => "index"
+  get "/tacos", :controller => "tacos", 
+  :action => "index"
+  resources "companies"
+  resources "contacts"
 end
